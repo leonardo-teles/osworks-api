7d785c25a080df0097ffac312547f672f53ea80e
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-04-23 18:07:57 -0300

    Validando Cliente Inexistente e Preenchendo Dados do Cliente na Ordem de Serviço

--- src/main/java/com/algaworks/osworks/service/OrdemServicoService.java
+++ src/main/java/com/algaworks/osworks/service/OrdemServicoService.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.osworks.enums.StatusOrdemServico;
+import com.algaworks.osworks.exception.NegocioException;
+import com.algaworks.osworks.model.Cliente;
 import com.algaworks.osworks.model.OrdemServico;
+import com.algaworks.osworks.repository.ClienteRepository;
 import com.algaworks.osworks.repository.OrdemServicoRepository;
 
 @Service
@@ -15,8 +18,15 @@ public class OrdemServicoService {
 	@Autowired
 	private OrdemServicoRepository servicoRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	public OrdemServico criar(OrdemServico ordemServico) {
 		
+		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId())
+				.orElseThrow(() -> new NegocioException("Cliente não encontrado"));
+		
+		ordemServico.setCliente(cliente);
 		ordemServico.setStatus(StatusOrdemServico.ABERTA);
 		ordemServico.setDataAbertura(LocalDateTime.now());
 		

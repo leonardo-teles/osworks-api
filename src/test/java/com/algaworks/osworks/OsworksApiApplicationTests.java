1f4b35ed6753a41a22ed8c535a1912fb87d8351c
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-04-25 02:38:28 -0300

    Alteração na Classe de Teste e Mudança na Dependência de Teste no pom.xml

--- src/test/java/com/algaworks/osworks/OsworksApiApplicationTests.java
+++ src/test/java/com/algaworks/osworks/OsworksApiApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OsworksApiApplicationTests {
+public class OsworksApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
